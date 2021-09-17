e0b557ea60e279febac89ac91d4f26af12f983df
Felipe Maia <dev.felipemaia@gmail.com>
2021-09-17 10:18:31 -0300

    finished activity

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,29 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from '../components/Button';
+import { GenreResponseProps } from '../App'
+interface SideBarProps {
+  genres: Array<GenreResponseProps>
+  handleClick: any;
+  selectedGenre: any;
+}
+
+export function SideBar({ genres, handleClick, selectedGenre }: SideBarProps) {
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClick(genre)}
+            selected={selectedGenre === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
 }
